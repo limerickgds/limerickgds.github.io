6f1f730352818f59a45293aeee5e82b05f20e9ca
limerickgds <619650072@qq.com>
2016-03-24 20:12:08 +0800

    配置了本地的jekyll环境，增加了gulp
    本地开发

---
+++ src/development/gulpfile.js
@@ -0,0 +1,7 @@
+// Generated on 2016-01-07 using generator-angular 0.15.1
+'use strict';
+
+
+ //引入register，但是仅仅引入这个不行，需要添加babel-preset-2015,并且配置.babelrc
+require('babel-core/register');
+require('./gulpfile.es6.js');
